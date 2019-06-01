bcdfe02b9ce3f45fce3bf0b7d98f0341b414e976
python <fuqiang6261@163.com>
2019-06-01 17:24:25 +0800

    first commit

---
+++ one.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import random
+
+# data = {k: chr(random.randint(65, 90)) for k in range(4) for _ in range(4)}
+# print(data)
+# k is index, v is value
+
+s1 = pd.Series([1, 2, 3, 4], index=['a', 'b', 'c', 'd'], name='A')
+s2 = pd.Series([i for i in range(5, 9)], index=['a', 'b', 'c', 'd'], name='B')
+# s3 = pd.Series(data, name='C')
+# print(s1)
+# print(s2)
+
+# 将series 组合为dataframe
+# NaN not a number
+df = pd.DataFrame({s1.name: s1, s2.name: s2})
+df1 = pd.DataFrame(s1, s2)
+# print(df)
+print(df1)
